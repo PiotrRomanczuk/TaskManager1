e7148bfb959731b2c84454f94048ad4a8ade941f
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-06-13 11:46:09 +0200

    Adding a basic Schema

--- server/server.js
+++ server/server.js
@@ -6,15 +6,26 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// Starting the server AFTER the connection with the database
+
+const startServer = async () => { 
+    try {
+        await connectDB();  
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+startServer();
+
+
+// Routes
+
 app.get('/', (req, res) => { 
     res.send("Home page") 
     console.log("Hello from the server")
 })
 
-connectDB()
-
-app.listen(PORT, () => { 
-    console.log(`Server running on port ${PORT}`)
-})
-
-// mongodb+srv://promanczuk: 1234@taskmanager.9ufphzl.mongodb.net /? retryWrites = true & w=majority;
