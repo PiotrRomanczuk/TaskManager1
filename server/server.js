f0626d602352394bc64c49b687de23f508ebf69d
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-07-14 23:28:55 +0200

    .

--- server/server.js
+++ server/server.js
@@ -5,34 +5,32 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const Task = require('./models/taskModel');
-
 const taskRoute = require('./routes/taskRoute');
 
 // Starting the server AFTER the connection with the database
 
-const startServer = async () => { 
-    try {
-        await connectDB();  
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`)
-        })
-    } catch (err) {
-        console.log(err)
-    }
-}
+const startServer = async () => {
+	try {
+		await connectDB();
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
 startServer();
 
 // Midlleware
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/tasks", taskRoute)
+app.use('/api/tasks', taskRoute);
 
 // Routes
-app.get('/', (req, res) => { 
-    res.send("Home page") 
-    console.log("Hello from the server")
-})
+app.get('/', (req, res) => {
+	res.send('Home page');
+	console.log('Hello from the server');
+});
 
-module.exports = {app}
-
+module.exports = { app };
