68d54334a636131acd397b2fa99cd5769ad9222c
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-08-18 17:22:25 +0200

    .

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -15,19 +15,4 @@ router.route('/').get(getAllTasks).post(createTask);
 
 router.route('/:id').get(getTaskByID).put(updateTask).delete(deleteTask);
 
-// //  Create a Task
-// router.post('/', createTask);
-
-// // Get/Read All Tasks
-// router.get('/', getAllTasks);
-
-// // Get/Read one Tasks
-// router.get('/:id', getTaskByID);
-
-// // Update a Task
-// router.put('/:id', updateTask);
-
-// // Delete a Task
-// router.delete('/:id', deleteTask);
-
 module.exports = router;
